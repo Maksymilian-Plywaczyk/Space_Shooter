a71266d04b775aea2063e7581cbf03b3755e5380
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-05-25 17:31:08 +0200

    solving problem with bullets position

--- Space_Shooter/Space_Shooter.cpp
+++ Space_Shooter/Space_Shooter.cpp
@@ -7,16 +7,19 @@ int main()
     const unsigned int width = 1000;
     const unsigned int height = 1000;
     sf::RenderWindow window(sf::VideoMode(width, height), "SpaceShooter");
-    sf::Vector2f position(width/2,height-31);
+    sf::Vector2f position(width/2,height-23);
     sf::Vector2f position_map(0,0);
+    sf::Vector2f position_bullet(200, 200);
     SpaceShip spaceship(position);
     Map map1(position_map);
+    std::vector<Bullet*>bullets;
+    bool isShooting = false;
     
     window.setFramerateLimit(60);
     sf::Clock clock;
     while (window.isOpen())
     {
-        
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -27,7 +30,24 @@ int main()
         spaceship.SpaceShip_animate(elapsed);
         window.clear();
         window.draw(map1);
-        window.draw(spaceship);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+        {
+            isShooting = true;
+        }
+        if (isShooting == true) {
+           
+            bullets.push_back(new Bullet(spaceship.getPosition().x+spaceship.getBounds().width,spaceship.getPosition().y
+                    +spaceship.getBounds().height));
+            isShooting = false;
+      }
+        for (int i = 0; i < bullets.size(); ++i)
+        {
+            bullets[i]->bullet_draw(window);
+            bullets[i]->bullet_shooting();
+            
+        }
+        spaceship.SpaceShip_draw(window);
+     
         
         window.display();
     }
