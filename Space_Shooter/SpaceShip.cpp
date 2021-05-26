ae81d06d3df9ffd3bbb88a47b65ed022393e0cf3
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-05-26 18:58:02 +0200

    Change Origin to correct

--- Space_Shooter/SpaceShip.cpp
+++ Space_Shooter/SpaceShip.cpp
@@ -1,53 +1,97 @@
 #include "SpaceShip.h"
+#include "Bullet.h"
 
-SpaceShip::SpaceShip(sf::Vector2f& position_) :position(position_){
+SpaceShip::SpaceShip(sf::Vector2f position_) :position( position_){
 	
-	this->setPosition(position_); //set Position of SpaceShip
+
 	if (!this->spaceship_texture.loadFromFile("textures/spaceship.png")) {
 		std::cout << "Blad odczytu pliku" << std::endl;
 	}
 	else
 		std::cout << "Poprawne wczytanie pliku" << std::endl;
-	auto textureSize = this->spaceship_texture.getSize();
+	
 	this->spaceship_texture.setSmooth(true);
-	this->setTexture(spaceship_texture);
-	this->setScale(1.5, 1.5);
-	this->setOrigin(this->getGlobalBounds().width / 2, this->getGlobalBounds().height / 2);//Set Origin to middle of sprite
-	std::cout <<"Size x:"<< spaceship_texture.getSize().x <<" Size y:"<< spaceship_texture.getSize().y << std::endl;
+	ship.setTexture(spaceship_texture);
+	ship.setOrigin(ship.getGlobalBounds().width / 2, ship.getGlobalBounds().height / 2);
+	ship.setScale(1.5, 1.5);
+
+	std::cout <<"Origin x: "<< ship.getOrigin().x<<"Origin y:"<< ship.getOrigin().y << std::endl;
+	ship.setPosition(position_);
+	std::cout << spaceship_texture.getSize().x << spaceship_texture.getSize().y << std::endl;
 }
 
 void SpaceShip::SpaceShip_animate(sf::Time& elapsed)
 {
-	sf::FloatRect spaceship_bounds = getGlobalBounds();
-	std::cout << spaceship_bounds.left + spaceship_bounds.width << " " << spaceship_bounds.top + spaceship_bounds.height << std::endl;
+	sf::FloatRect spaceship_bounds = ship.getGlobalBounds();
+
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
 		if (spaceship_bounds.left >= 0)
 		{
-			this->move(-speed_of_direction_x * elapsed.asSeconds(),0);
+			ship.move(-speed_of_direction_x * elapsed.asSeconds(),0);
 		}
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
 		if (spaceship_bounds.top >= 0)
 		{
-			this->move(0, -speed_of_direction_y*elapsed.asSeconds());
+			ship.move(0, -speed_of_direction_y*elapsed.asSeconds());
 		}
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
 		if (spaceship_bounds.left+spaceship_bounds.width <= 1000)
 		{
-			this->move(speed_of_direction_x * elapsed.asSeconds(), 0);
+			ship.move(speed_of_direction_x * elapsed.asSeconds(), 0);
 		}
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 	{
 		if (spaceship_bounds.top+spaceship_bounds.height <= 1000)
 		{
-			this->move(0, speed_of_direction_y * elapsed.asSeconds());
+			ship.move(0, speed_of_direction_y * elapsed.asSeconds());
 		}
 	}
 
 
 }
+
+void SpaceShip::SpaceShip_draw(sf::RenderWindow& window)
+{
+	window.draw(ship);
+}
+
+const sf::FloatRect SpaceShip::getBounds()
+{
+	return ship.getGlobalBounds();
+}
+
+const sf::Vector2f SpaceShip::getPosition()
+{
+	return ship.getPosition();
+}
+
+
+
+//void SpaceShip::shipShooting(std::vector<Bullet*>& bullets, sf::RenderWindow& window)
+//{
+//
+//	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+//	{
+//		isShooting = true;
+//	}
+//	if (isShooting == true) {
+//
+//		bullets.push_back(new Bullet(position));
+//		isShooting = false;
+//	}
+//	for (int i = 0; i < bullets.size(); ++i)
+//	{
+//		bullets[i]->bullet_draw(window);
+//		bullets[i]->bullet_shooting();
+//	}
+//
+//}
+
+
+
--- Space_Shooter/SpaceShip.h
+++ Space_Shooter/SpaceShip.h
@@ -1,21 +1,31 @@
 #pragma once
 
+
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
 #include <memory>
+#include "Bullet.h"
 
-class SpaceShip :public sf::Sprite
+class SpaceShip
 {
 private:
+	sf::Sprite ship;
 	sf::Texture spaceship_texture;
 	sf::Vector2f position;
-	const  int speed_of_direction_x = 250;
-	const  int speed_of_direction_y = 250;
+	const  int speed_of_direction_x = 400;
+	const  int speed_of_direction_y = 400;
+	bool isShooting=false;
+	
 
 public:
-	SpaceShip(sf::Vector2f& position_);
+	
+	SpaceShip(sf::Vector2f position_);
 	void SpaceShip_animate(sf::Time& elapsed);
+	void SpaceShip_draw(sf::RenderWindow& window);
+	const sf::FloatRect getBounds();
+	const sf::Vector2f getPosition();
+	//void shipShooting(std::vector<Bullet*>&bullets,sf::RenderWindow& window);
 
 };
 
