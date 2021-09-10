fed23c3f1eb7d6b3ff4ddc59a5f1cc7b24269051
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-09-10 13:07:53 +0200

    Spawning enemies

--- Space_Shooter/Enemies.cpp
+++ Space_Shooter/Enemies.cpp
@@ -19,3 +19,43 @@ void Enemies::enemies_draw(sf::RenderWindow& window)
 {
 	window.draw(enemies);
 }
+
+void Enemies::enemies_bounds(sf::RenderWindow& window,sf::Time&elapsed)
+{
+	auto enemies_bounds = enemies.getGlobalBounds();
+
+	if (enemies_bounds.left<=0)
+	{
+		enemies_speed_x = std::abs(enemies_speed_x);
+	}
+	if (enemies_bounds.left + enemies_bounds.width >= window.getSize().x)
+	{
+		enemies_speed_x=-std::abs(enemies_speed_x);
+	}
+	enemies.move(enemies_speed_x * elapsed.asSeconds(), enemies_speed_y * elapsed.asSeconds());
+}
+
+void Enemies::enemies_animation(sf::RenderWindow& window, sf::Time& elapsed)
+{
+	if (EnemiesSpawnTime < 50)
+	{
+		EnemiesSpawnTime++;
+	}
+	if (EnemiesSpawnTime >= 50)
+	{
+		sf::Vector2f position(std::rand() % (window.getSize().x),0);
+		enemies_.push_back(new Enemies(position));
+		EnemiesSpawnTime = 0;
+	}
+	for (auto i = 0; i < enemies_.size(); i++)
+	{
+		enemies_[i]->enemies_bounds(window, elapsed);
+	}
+	for (auto i = 0; i <enemies_.size(); i++)
+	{
+		enemies_[i]->enemies_draw(window);
+	}
+}
+
+
+
--- Space_Shooter/Enemies.h
+++ Space_Shooter/Enemies.h
@@ -9,10 +9,19 @@ private:
 	sf::Sprite enemies;
 	sf::Texture enemies_texture;
 	sf::Vector2f position;
+	 int enemies_speed_x = 0;
+	 int enemies_speed_y = 100;
+	 std::vector<Enemies*>enemies_;
+	 int EnemiesSpawnTime = 0;
+
 
 public:
 	Enemies(sf::Vector2f position_);
 	void enemies_draw(sf::RenderWindow& window);
+	void enemies_bounds(sf::RenderWindow& window,sf::Time&elapsed);
+	void enemies_animation(sf::RenderWindow& window, sf::Time& elapsed);
+
+
 
 
 
