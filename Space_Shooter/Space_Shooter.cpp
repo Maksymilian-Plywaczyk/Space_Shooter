28dfd97051546953a59fac38eb828e80da6f3dfd
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-01-11 21:36:35 +0100

    Replace main to game.cpp and game.h

--- Space_Shooter/Space_Shooter.cpp
+++ Space_Shooter/Space_Shooter.cpp
@@ -1,51 +1,10 @@
-﻿#include <SFML/Graphics.hpp>
-#include "SpaceShip.h"
-#include "Map.h"
-#include "Bullet.h"
-#include "Enemies.h"
+﻿#include "Game.h"
 
 int main()
 {
-    const unsigned int width = 1000;
-    const unsigned int height = 1000;
-    sf::RenderWindow window(sf::VideoMode(width, height), "SpaceShooter");
-    
-    sf::Vector2f position_map(0,0);
-    sf::Vector2f position_bullet(200, 200);
-    SpaceShip spaceship(sf::Vector2f(width/2,height-42));
-    Map map1(position_map);
-    Bullet bullet(sf::Vector2f(0, 0));
-    Enemies enemies(sf::Vector2f(200.0, 300.0));
-    window.setFramerateLimit(60);
-    sf::Clock clock;
-  
-    while (window.isOpen())
-    {
-        
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-     
-        sf::Time elapsed = clock.restart();
-        
-        spaceship.SpaceShip_animate(elapsed);
-        //enemies.enemies_bounds(window,elapsed);
-        window.clear();
-        window.draw(map1);
-
-        bullet.player_shooting(window, spaceship);
-        enemies.enemies_animation(window, elapsed,spaceship,bullet.bullets,bullet.bullets2);
-        
-
-       
-        spaceship.SpaceShip_draw(window);
-    
-
-        window.display();
-    }
-
-    return 0;
+	Game game;
+	
+	game.start();
+	
+	return 0;
 }
